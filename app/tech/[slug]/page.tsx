e762d6e2b3349b075fcec9c9efa3f6ba73a1c74e
ukkodeveloper <ukkodeveloper@gmail.com>
2024-01-09 00:51:24 +0900

    feat: render MDX file
    - custom MDX component

--- app/tech/[slug]/page.tsx
+++ app/tech/[slug]/page.tsx
@@ -1,4 +1,8 @@
 import { allTechPosts } from '@/.contentlayer/generated';
+import { notFound } from 'next/navigation';
+import { useMDXComponent } from 'next-contentlayer/hooks';
+import type { MDXComponents } from 'mdx/types';
+import Link from 'next/link';
 
 interface PageProps {
   params: {
@@ -12,5 +16,19 @@ export async function generateStaticParams() {
 }
 
 export default function Page({ params: { slug } }: PageProps) {
-  return <div>{slug}</div>;
+  const post = allTechPosts.find((post) => post.slug === slug);
+
+  if (!post) notFound();
+
+  const MDXContent = useMDXComponent(post.body.code);
+
+  const mdxComponents: MDXComponents = {
+    a: ({ href, children }) => (
+      <Link className="text-red-700" href={href ?? ''}>
+        {children}
+      </Link>
+    ),
+  };
+
+  return <MDXContent components={mdxComponents} />;
 }
