d54793c7bebf872d545bff43332aeaa7e4d5e039
ukkodeveloper <ukkodeveloper@gmail.com>
2024-01-08 18:38:19 +0900

    chore: configures to generate conetentlayer from mdx files

---
+++ contentlayer.config.ts
@@ -0,0 +1,48 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+
+const TechPost = defineDocumentType(() => ({
+  name: 'TechPost',
+  filePathPattern: 'tech/**/*.mdx',
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+    tags: { type: 'list', of: { type: 'string' }, required: true },
+    published: { type: 'boolean', required: false, default: false },
+    summary: { type: 'string', required: false },
+    images: {
+      type: 'list',
+      of: {
+        type: 'string',
+      },
+      default: [],
+      required: false,
+    },
+  },
+}));
+
+const LifePost = defineDocumentType(() => ({
+  name: 'LifePost',
+  filePathPattern: 'life/**/*.mdx',
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+    tags: { type: 'list', of: { type: 'string' }, required: true },
+    published: { type: 'boolean', required: false, default: false },
+    summary: { type: 'string', required: false },
+    images: {
+      type: 'list',
+      of: {
+        type: 'string',
+      },
+      default: [],
+      required: false,
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: 'content',
+  documentTypes: [TechPost, LifePost],
+});
