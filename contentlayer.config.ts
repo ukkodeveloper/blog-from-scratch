21f872770a53079058fea365e7885d45e708b7ce
ukkodeveloper <ukkodeveloper@gmail.com>
2024-01-09 14:22:51 +0900

    feat: apply remark and rehype plugins

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -4,6 +4,11 @@ import {
   defineDocumentType,
   makeSource,
 } from 'contentlayer/source-files';
+import remarkGfm from 'remark-gfm';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypePrismPlus from 'rehype-prism-plus';
+import rehypeCodeTitles from 'rehype-code-titles';
 
 const fields: FieldDefs = {
   title: { type: 'string', required: true },
@@ -43,4 +48,13 @@ const LifePost = defineDocumentType(() => ({
 export default makeSource({
   contentDirPath: 'content',
   documentTypes: [TechPost, LifePost],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      rehypeAutolinkHeadings,
+      rehypeCodeTitles,
+      [rehypePrismPlus, { defaultLanguage: 'ts' }],
+    ],
+  },
 });
