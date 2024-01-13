9ef6ee47394ea08e6ecedd5112c9751b293d5666
ukkodeveloper <ukkodeveloper@gmail.com>
2024-01-13 12:07:13 +0900

    feat: set layout Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 const Header = () => {
   return (
     <header className="sticky top-0 z-10">
-      <nav className="flex items-center bg-neutral-800 px-1 py-2.5 lg:py-3.5">
+      <nav className="flex items-center justify-between bg-neutral-800 px-1 py-2.5 lg:py-3.5">
         <Link className="inline-block " href={'/tech/example'}>
           <Image
             alt={'로고'}
@@ -15,9 +15,12 @@ const Header = () => {
             className="w-32 lg:w-48"
           />
         </Link>
-        <Link className="inline-block" href={'/tech/example'}>
-          블로그
-        </Link>
+        <div className="flex gap-4 text-xl font-bold text-primary-100">
+          <Link href={'/tech/example'}>블로그</Link>
+          <Link className="text-primary-200" href={'/tech/example'}>
+            포트폴리오
+          </Link>
+        </div>
       </nav>
     </header>
   );
