448446c2a4ae9ac7c12e859373efb8b2ec8bbcd6
ukkodeveloper <ukkodeveloper@gmail.com>
2024-01-13 11:50:32 +0900

    feat: set logo img in Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,19 @@
 import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
 
 const Header = () => {
   return (
-    <header>
-      <nav className="flex h-60 bg-cyan-300">
-        <Link className="inline-block bg-amber-500" href={'/tech/example'}>
-          로고
+    <header className="sticky top-0 z-10">
+      <nav className="flex items-center bg-neutral-800 px-1 py-2.5 lg:py-3.5">
+        <Link className="inline-block " href={'/tech/example'}>
+          <Image
+            alt={'로고'}
+            src={'/logo.png'}
+            width={192}
+            height={100}
+            className="w-32 lg:w-48"
+          />
         </Link>
         <Link className="inline-block" href={'/tech/example'}>
           블로그
