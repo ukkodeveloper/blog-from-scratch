b2f13aea69af27e7d1e1af97b3c86f01498068f0
ukkodeveloper <ukkodeveloper@gmail.com>
2024-01-14 10:37:59 +0900

    feat: refactor Header styling and remove width/height attributes
    - it's local image. if import image module, it doesn't make any layout shift

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,14 @@
 import Link from 'next/link';
 import React from 'react';
 import Image from 'next/image';
+import logo from '../public/logo.svg';
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-10 bg-neutral-800">
-      <nav className="m-auto flex max-w-screen-laptop items-center justify-between px-6 ">
+    <header className="sticky top-0 z-10 bg-neutral-800 py-4">
+      <nav className="m-auto flex max-w-screen-laptop items-center justify-between px-6  ">
         <Link className="inline-block " href={'/tech/example'}>
-          <Image
-            alt={'로고'}
-            src={'/logo.svg'}
-            width={144}
-            height={25.88}
-            className="lg:w-48 w-32"
-          />
+          <Image alt={'로고'} src={logo} className="lg:w-48 w-32" />
         </Link>
         <div className="flex gap-4 text-neutral-50">
           <Link href={'/tech/example'}>블로그</Link>
