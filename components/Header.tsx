d009e726823ab3cbc8ceca50b7f3206179e27dd8
ukkodeveloper <ukkodeveloper@gmail.com>
2024-01-14 17:58:24 +0900

    feat: use icons in Header
    - search
    - tags

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,17 +2,25 @@ import Link from 'next/link';
 import React from 'react';
 import Image from 'next/image';
 import logo from '../public/logo.svg';
+import { TagIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-10 bg-neutral-800 py-4">
-      <nav className="m-auto flex max-w-screen-laptop items-center justify-between px-6  ">
-        <Link className="inline-block " href={'/tech/example'}>
-          <Image alt={'로고'} src={logo} className="lg:w-48 w-32" />
+    <header className="sticky top-0 z-10 bg-neutral-700/30 backdrop-blur-lg backdrop-filter ">
+      <nav className="m-auto flex max-w-screen-laptop items-end justify-between px-6 py-2 laptop:py-2.5">
+        <Link href="/">
+          <Image alt="로고" src={logo} className="w-44 laptop:w-56" />
         </Link>
-        <div className="flex gap-4 text-neutral-50">
-          <Link href={'/tech/example'}>블로그</Link>
-          <Link href={'/tech/example'}>나에 대해</Link>
+        <div className="flex items-end gap-4 text-neutral-50">
+          <Link href="/">만들기</Link>
+          <Link href="/">학습하기</Link>
+          <Link href="/">회고하기</Link>
+          <Link href="/" className=" border-spacing-1.5 rounded-lg pb-1">
+            <TagIcon className="h-5 w-5 text-neutral-50" />
+          </Link>
+          <Link href="/" className=" border-spacing-1.5 rounded-lg pb-1">
+            <MagnifyingGlassIcon className="h-5 w-5 text-neutral-50" />
+          </Link>
         </div>
       </nav>
     </header>
