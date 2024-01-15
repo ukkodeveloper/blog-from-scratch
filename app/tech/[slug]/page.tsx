bd6f2f9554421083be2179417a613113c9c66e56
ukkodeveloper <ukkodeveloper@gmail.com>
2024-01-15 16:34:48 +0900

    feat: apply utility functions

--- app/tech/[slug]/page.tsx
+++ app/tech/[slug]/page.tsx
@@ -1,8 +1,8 @@
-import { allTechPosts } from '@/.contentlayer/generated';
 import { notFound } from 'next/navigation';
 import { useMDXComponent } from 'next-contentlayer/hooks';
 import type { MDXComponents } from 'mdx/types';
 import Link from 'next/link';
+import { getPostBySlug, sortedPosts } from '@/lib/posts';
 
 interface PageProps {
   params: {
@@ -11,11 +11,11 @@ interface PageProps {
 }
 
 export async function generateStaticParams() {
-  return allTechPosts.map((post) => post.slug);
+  return sortedPosts.map((post) => post.slug);
 }
 
 export default function Page({ params: { slug } }: PageProps) {
-  const post = allTechPosts.find((post) => post.slug === slug);
+  const post = getPostBySlug(slug);
 
   if (!post) notFound();
 
