04c76f867ad93aecf3aa6ae6fa5059377da9be0e
ukkodeveloper <ukkodeveloper@gmail.com>
2024-03-15 19:09:41 +0900

    design: badge hover style

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -8,7 +8,7 @@ interface BadgeProps {
 }
 
 const Badge = ({ name, variant, selected = false }: BadgeProps) => {
-  const baseStyle = `border border-2 border-neutral-800 px-2 py-1 text-[10px] uppercase ${SHAPE[variant]}`;
+  const baseStyle = `border border-2 border-neutral-800 px-2 py-1 text-[10px] uppercase hover:bg-primary-300 ${SHAPE[variant]}`;
   const link =
     variant === 'TAG'
       ? encodeURI(`/tags/${name}`)
@@ -28,6 +28,6 @@ const Badge = ({ name, variant, selected = false }: BadgeProps) => {
 export default Badge;
 
 const SHAPE = {
-  TAG: 'rounded-full hover:bg-yellow-300',
-  SERIES: 'rounded-md hover:bg-primary-300',
+  TAG: 'rounded-full',
+  SERIES: 'rounded-md',
 };
