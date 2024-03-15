829cd9d4e1f7e0e1ffd618de06e418b8edf5730c
ukkodeveloper <ukkodeveloper@gmail.com>
2024-03-15 19:09:25 +0900

    feat: remove logo in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,17 @@
 import Link from 'next/link';
 import React from 'react';
-import Image from 'next/image';
-import logo from '../public/static/logo.svg';
 import { TagIcon, RectangleStackIcon } from '@heroicons/react/24/outline';
 
 const Header = () => {
   return (
-    <header className="sticky top-0  z-10  bg-neutral-700/30 backdrop-blur-lg backdrop-filter ">
-      <nav className="laptop:py-2.5 m-auto flex max-w-screen-xl items-end justify-between px-6 py-2">
-        <Link href="/">
-          <Image alt="로고" src={logo} className="w-44 xl:w-56" />
-        </Link>
-        <div className="flex items-end gap-4 text-neutral-50">
-          <Link
-            href={'/series'}
-            className=" border-spacing-1.5 rounded-lg pb-1"
-          >
-            <RectangleStackIcon className="h-6 w-6 text-neutral-50 xl:h-8  xl:w-8" />
-          </Link>
-          <Link href={'/tags'} className=" border-spacing-1.5 rounded-lg pb-1">
-            <TagIcon className="h-6 w-6 text-neutral-50 xl:h-8  xl:w-8" />
-          </Link>
-        </div>
-      </nav>
-    </header>
+    <nav className="flex w-full justify-end gap-4 px-6 py-2 text-primary">
+      <Link href={'/series'}>
+        <RectangleStackIcon className="h-7 w-7 text-primary" />
+      </Link>
+      <Link href={'/tags'}>
+        <TagIcon className="h-7 w-7 text-primary" />
+      </Link>
+    </nav>
   );
 };
 
