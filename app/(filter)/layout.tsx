6dd86c85467803943b4d5654bc6859c3747c360c
ukkodeveloper <ukkodeveloper@gmail.com>
2024-03-16 11:45:37 +0900

    chore: delete post layout which has padding(x axis)

--- app/(filter)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import type { ReactNode } from 'react';
-
-const Layout = ({ children }: { children: ReactNode }) => {
-  return <div className="px-10">{children}</div>;
-};
-
-export default Layout;
