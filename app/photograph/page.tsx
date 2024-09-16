d733806a1acae971e5be9192c4ace2c98e77062d
ukkodeveloper <ukkodeveloper@gmail.com>
2024-09-16 17:29:06 +0900

    feat: photo

--- app/photograph/page.tsx
+++ app/photograph/page.tsx
@@ -1,15 +1,55 @@
-function PhotographPage() {
+'use client';
+
+import Txt from '@/components/Txt';
+import _projectImages from './(data)/projectImages.json';
+
+import Image from 'next/image';
+
+const PhotographerPortfolio = () => {
+  const projectImages = _projectImages as Record<string, string[]>;
+  const projects = Object.keys(projectImages);
+
   return (
-    <div>
-      <h1>Photograph</h1>
-      <p>Photograph page content</p>
-      <img
-        src="/path/to/fullsize-image.jpg"
-        alt="Full size"
-        style={{ width: '100%', height: 'auto' }}
-      />
+    <div className="h-[100svh] overflow-y-hidden">
+      <div className="w-max-[650px]">
+        <Txt as="h1" fontSize="2xl" className="px-4 py-2">
+          _wrks
+        </Txt>
+        <div className=" h-[100svh] overflow-y-auto">
+          {projects.map((project, index) => (
+            <div
+              key={project + index}
+              className="group relative mx-2 flex w-full flex-shrink-0"
+            >
+              <Image
+                width={650}
+                height={1400}
+                src={projectImages[project][0]}
+                alt={`Gallery image ${project}`}
+                className="w-full object-cover transition duration-500 group-hover:scale-90"
+              />
+              <div className="absolute right-3 top-10">
+                {[...project].map((str, index) => (
+                  <Txt
+                    key={str + index}
+                    className="text-s m-auto w-fit text-black opacity-0 transition duration-500 group-hover:opacity-100"
+                  >
+                    {str}
+                  </Txt>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+        <Txt
+          fontSize="xl"
+          className="absolute bottom-4 right-10 from-neutral-400 font-wSans text-white"
+        >
+          KIM YOOKWON
+        </Txt>
+      </div>
     </div>
   );
-}
+};
 
-export default PhotographPage;
+export default PhotographerPortfolio;
--- components/PageContainer.tsx
+++ components/PageContainer.tsx
@@ -6,8 +6,10 @@ interface Props {
 
 export default function PageContainer({ children }: Props) {
   return (
-    <main className="relative mx-auto my-6 h-max min-h-screen max-w-[650px] space-y-6 px-3 sm:my-10 sm:px-4">
-      {children}
+    <main className="h-[100svh] overflow-y-auto">
+      <div className="relative mx-auto h-max min-h-screen max-w-[650px] space-y-6 px-3">
+        {children}
+      </div>
     </main>
   );
 }
