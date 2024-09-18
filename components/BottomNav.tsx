78add6fb5cd90227147c6b8812e3b8b4ecc65cd8
ukkodeveloper <ukkodeveloper@gmail.com>
2024-09-18 23:11:54 +0900

    fix: bottom nav

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 import { twMerge } from 'tailwind-merge';
+import Txt from './Txt';
 
 const BottomNav = () => {
   const pathName = usePathname();
@@ -12,15 +13,19 @@ const BottomNav = () => {
 
   const linkClass = (isActive: boolean) =>
     twMerge(
-      'transition-color rounded-md p-2 text-xs font-semibold text-gray-700 duration-300 hover:bg-gray-800 hover:text-white md:text-sm',
-      isActive ? 'bg-gray-800 text-white cursor-default' : 'cursor-pointer'
+      'transition-color rounded-xl p-1 text-xs font-semibold text-gray-700 duration-300 hover:bg-gray-800 hover:text-white md:text-sm',
+      isActive ? 'bg-gray-200 text-white cursor-default' : 'cursor-pointer'
     );
 
   return (
     <div className="z-100 fixed bottom-4 left-5 flex items-center rounded-xl border border-neutral-200 bg-white px-2 py-1 text-xs drop-shadow-2xl">
       <Link href="/" className={linkClass(isBlog)}>
-        <span className="hidden sm:inline">_blog</span>
-        <span className="sm:hidden">_b</span>
+        <Txt fontSize="sm" className="hidden sm:inline">
+          _blog
+        </Txt>
+        <Txt fontSize="sm" className="p-0 sm:hidden">
+          _b
+        </Txt>
       </Link>
       <span className="px-2">|</span>
       <Link
@@ -28,8 +33,12 @@ const BottomNav = () => {
         className={linkClass(isPhotographPage)}
         onClick={(e) => isPhotographPage && e.preventDefault()}
       >
-        <span className="hidden sm:inline">_wrks</span>
-        <span className="sm:hidden">_w</span>
+        <Txt fontSize="sm" className="hidden sm:inline">
+          _wrks
+        </Txt>
+        <Txt fontSize="sm" className="p-0 sm:hidden ">
+          _w
+        </Txt>
       </Link>
       <span className="px-2">|</span>
     </div>
